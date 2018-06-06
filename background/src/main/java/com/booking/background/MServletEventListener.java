5d360501c740be1fd2ecdfe85d348c5355734ee6
kai.liu <469912181@qq.com>
2018-06-06 18:05:26 +0800

    删除无用倒包

--- background/src/main/java/com/booking/background/MServletEventListener.java
+++ background/src/main/java/com/booking/background/MServletEventListener.java
@@ -1,29 +1,13 @@
 package com.booking.background;
 
 import com.booking.background.service.NotHandledOrderService;
-import com.booking.background.websocket.WebsocketController;
-import com.booking.common.dto.OrderDetailDto;
-import com.booking.common.entity.OrderDetailEntity;
-import com.booking.common.entity.OrderEntity;
-import com.booking.common.entity.ShopEntity;
-import com.booking.common.mapper.OrderMapper;
-import com.booking.common.mapper.OrderShopRelMapper;
-import com.booking.common.service.IShopService;
 import com.opdar.platform.core.base.Context;
 import com.opdar.platform.core.base.ServletEventListener;
-import com.opdar.plugins.mybatis.core.SqlSessionFactoryBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
-import org.springframework.web.socket.WebSocketSession;
 
 import javax.servlet.ServletContextEvent;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * MServletEventListener
