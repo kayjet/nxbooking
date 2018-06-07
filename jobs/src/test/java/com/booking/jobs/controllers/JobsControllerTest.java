3e1a7e42b41469a315c26d59e7993f2036115350
kai.liu <469912181@qq.com>
2018-06-07 16:55:17 +0800

    Junit

--- jobs/src/test/java/com/booking/jobs/controllers/JobsControllerTest.java
+++ jobs/src/test/java/com/booking/jobs/controllers/JobsControllerTest.java
@@ -1,5 +1,35 @@
+package com.booking.jobs.controllers;
+
+import com.alibaba.fastjson.JSON;
+import com.booking.common.entity.OrderEntity;
+import com.booking.common.mapper.OrderMapper;
+import com.booking.common.quartz.MyQuartzExecutorDelegate;
+import com.booking.common.utils.NetTool;
+import com.booking.jobs.AbstractTransactionalTestService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import static org.junit.Assert.*;
 
-public class JobsControllerTest {
+
+public class JobsControllerTest extends AbstractTransactionalTestService {
+    @Autowired
+    OrderMapper orderMapper;
+
+    @Autowired
+    MyQuartzExecutorDelegate myQuartzExecutorDelegate;
+
+    @org.junit.Test
+    public void addJob() throws Exception {
+
+//        OrderEntity orderEntity = orderMapper.selectOne(new OrderEntity("0b4c0cf4-c3e3-4414-a6d5-722576d7e7c9"));
+        OrderEntity orderEntity = orderMapper.selectOne(new OrderEntity("1582c0f0-0abf-491c-9697-09141632590e"));
+        myQuartzExecutorDelegate.addCloseOrderJob(orderEntity);
+    }
+
+
+    @org.junit.Test
+    public void removeob() throws Exception {
+        myQuartzExecutorDelegate.removeCloseOrderJob("1582c0f0-0abf-491c-9697-09141632590e");
+    }
 
 }
